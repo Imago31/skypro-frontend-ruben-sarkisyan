786e041bcf254009686675433e4b6b359001ed3f
Ruben Sarkisyan <41841660+Imago31@users.noreply.github.com>
2021-11-15 17:56:48 +0200

    hm1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-let itemName='Кресло';
-let itemPrice='1000';
-let itemResoult=`${itemName}, цена ${itemPrice} рублей`;
+let itemName = 'Кресло';
+let itemPrice = '1000';
+let itemResoult = `${itemName}, цена ${itemPrice} рублей`;
 alert(itemResoult);
